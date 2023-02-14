db5b594a42fc764a0eef2f3349fb63869adc7bcb
Leila Moraes <leila.violino@gmail.com>
2023-02-14 02:25:52 -0300

    feat: cria arquivo com lógica para a validação do talk.rate

---
+++ src/middlewares/rateValidation.js
@@ -0,0 +1,16 @@
+function rateValidation(req, res, next) {
+    const { talk } = req.body;
+    const { rate } = talk;
+    const ERROR_MESSAGE = 400;
+
+    if (!rate) {
+        return res.status(ERROR_MESSAGE).json({ message: 'O campo "rate" é obrigatório' }); 
+    }
+    if (rate % 1 !== 0 || rate < 1 || rate > 5) {
+        return res.status(ERROR_MESSAGE)
+        .json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
+    }
+    return next();
+}
+
+module.exports = rateValidation;
